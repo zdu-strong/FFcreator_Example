7f2fa4ca2a0afe6c3083ea60c993e3eb77c6e890
John <zdu.strong@gmail.com>
2025-01-11 22:34:40 +0900

    #: Add mirror

--- bin/pre_load_configuration.js
+++ bin/pre_load_configuration.js
@@ -22,6 +22,8 @@ async function installDependencies() {
       env: {
         ...process.env,
         "npm_config_package_lock": "false",
+        "npm_config_canvas_binary_host_mirror": "https://npmmirror.com/mirrors/canvas",
+        "npm_config_gl_binary_host_mirror": "https://repo.huaweicloud.com/gl",
       },
     }
   );
